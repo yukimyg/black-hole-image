40a6b67a90b50cc8ad93860e2499dcf142fdee22
Yuki Miyagi <61782737+yukimyg@users.noreply.github.com>
2022-02-16 23:21:53 +0900

    refactor: remove setting of test
    #3

--- set_values.py
+++ set_values.py
@@ -1,5 +1,5 @@
 from internal.datas import Const as const
-#from internal.datas import Image as image
+from internal.datas import Image as image
 
 
 mass_bh = 100 * const.mass_sun
@@ -8,8 +8,8 @@ mass_bh = 100 * const.mass_sun
 rs = 2 * mass_bh * const.newtonian_g / const.c ** 2
 distance = 5 * rs
 
-image_width = 4000#image.uhd.width
-image_height = 2000#image.uhd.height
+image_width = image.fhd.width
+image_height = image.fhd.height
 
 input_name = "input.png"
 output_name = "output.png"
